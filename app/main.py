fa1e95cac48b794bfc4899d4a6dba2c19ba63110
Den1403 <denis14031990denis@gmail.com>
2026-03-24 20:18:34 +0300

    Initial commit: URL shortener service

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
-from app.routers import links
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from .routers import links
 
 app = FastAPI(title="URL Shortener Service")
 
 
-app.include_router(links.router)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+@app.get("/ui")
+def ui():
+    return FileResponse("app/static/index.html")
 
 @app.get("/")
 def root():
     return {"message": "URL Shortener API is running"}
+
+
+app.include_router(links.router)
+
+
