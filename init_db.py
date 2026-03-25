411ddcc13aadc0dc9de6d1d0c21d26e0074cc83a
Den1403 <denis14031990denis@gmail.com>
2026-03-25 10:06:38 +0300

    feat: add Docker sup

--- init_db.py
+++ init_db.py
@@ -1,13 +1,13 @@
+import os
 import asyncio
 from app.database import engine, Base
-from app.models import Link 
 
 
 async def init():
+    os.makedirs("data", exist_ok=True)
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)  # удаляем табл
-        await conn.run_sync(Base.metadata.create_all)  
-    print("Database initialized!")
+        await conn.run_sync(Base.metadata.create_all)
+    print("Database initialized!!!!")
 
 
 if __name__ == "__main__":
